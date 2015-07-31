b115247e2f2065fb405a45f3bf0d0c86bc4365af
daoudi el hassan <elha-da@users.noreply.github.com>
2015-07-31 16:51:40 +0200

    ajout de fichiers js

--- 20150731/recherche.php
+++ 20150731/recherche.php
@@ -4,21 +4,23 @@
 include('template.php');
 // on créé une nouvelle instance de la classe Template
 // et on indique en argument le chemin vers les modèles
-$aside_recherche = new Template('./html/');
-$piedAside_recherche = new Template('./html/');
-$recherche = new Template('./html/');
-$onglets_recherche = new Template('./html/');
+$entete_site = new Template('./html/');
+$aside_recherche = new Template('./html/recherche/');
+$piedAside_recherche = new Template('./html/recherche/');
+$block_recherche = new Template('./html/recherche/');
+$onglets_recherche = new Template('./html/recherche/');
 
 // modèle à utiliser auquel on adjoint un nom arbitraire
+$entete_site->set_filenames(array('entete_site' => 'entete_site.html'));
 $aside_recherche->set_filenames(array('aside_recherche' => 'aside_recherche.html'));
-$piedAside_recherche->set_filenames(array('piedAside_recherche' => 'piedAside_recherche.html'));
-$recherche->set_filenames(array('recherche' => 'recherche.html'));
 $onglets_recherche->set_filenames(array('onglets_recherche' => 'onglets_recherche.html'));
+$block_recherche->set_filenames(array('block_recherche' => 'block_recherche.html'));
+$piedAside_recherche->set_filenames(array('piedAside_recherche' => 'piedAside_recherche.html'));
 
+$entete_site->pparse('entete_site');
 $aside_recherche->pparse('aside_recherche');
 $onglets_recherche->pparse('onglets_recherche');
-$recherche->pparse('recherche');
-
+$block_recherche->pparse('block_recherche');
 $piedAside_recherche->pparse('piedAside_recherche');
 
 ?>
