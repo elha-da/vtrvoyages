62578ce355bb61a98691e6203b791c747b8f9e4a
daoudi el hassan <elha-da@users.noreply.github.com>
2015-08-06 16:35:44 +0200

    rajout du fichier jtree.js pour le Jtree

---
+++ test_Jtree/jtree.js
@@ -0,0 +1,9 @@
+
+function choix(paysId, paysVal) {
+    var id = 'search_type_'+paysId ;
+    var valeur = paysVal ;
+    var bloc = document.getElementById(id).innerHTML;
+    $('#tree1').empty();
+    $('#tree1').append(bloc);
+}
+
